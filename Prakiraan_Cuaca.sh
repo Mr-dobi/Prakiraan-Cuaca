14ede594f59ac6a1a60ca352f6a531c4ab5c14aa
Reyhan Dwi <66651244+Mr-dobi@users.noreply.github.com>
2020-08-12 15:46:55 +0700

    Add files via upload

---
+++ Prakiraan_Cuaca.sh
@@ -0,0 +1,11 @@
+# hallo ngentod *-*
+# mau apa lo kesini mau recode ya
+# terus lo pamerin ke temen lo
+# klok lo hacker !
+# mimpi bosss
+# nih silahkan didecrypt saja
+# mudah tuh decrypt nya !
+
+z="
+";IBz='read';VBz='elas';az='33m=';bBz=' !\x';SBz='ika ';Xz='curl';uz='Cybe';yz='utub';EBz='m FB';cz='===\';Yz='91m+';Az='slee';cBz='1b[1';jBz='$kot';xz='m Yo';Sz='1b[3';NBz='anda';HBz='an8"';gBz='p://';Ez='echo';MBz='ota ';rz='m Te';Tz='7;1m';LBz='ut k';XBz='ahka';GBz='Mas.';bz='====';aBz='esar';Hz='b[1;';DBz='an"';lz=' \x1';dBz=';92m';Gz='"\x1';tz='96m ';Pz='lu b';UBz='ng j';fBz=' htt';dz='x1b[';FBz='    ';jz='m Au';Cz='clea';ABz='e\x1';pz='eyha';BBz='Mas ';Oz='n du';Qz='os q';nz=':\x1';iBz='.in/';Rz='ue\x';YBz='n di';kBz='a';qz='n"';Iz='91mS';fz='m|+"';ZBz='perb';kz='thor';wz='rmux';Jz='ilah';Zz='|\x1';Lz='inst';mz='91m ';PBz=' kot';TBz='kura';Fz=' -e ';hz='  *\';oz='Mr.R';iz='1;92';sz='am  ';WBz=' sil';KBz='"Inp';Uz='"';Kz='kan ';eBz='p 4';Vz='p 2';OBz=' : "';Dz='r';CBz='Reyh';Wz='pkg ';Nz='baha';Mz='all ';RBz='91mj';JBz=' -p ';ez='1;91';Bz='p 1';gz='33m ';hBz='wttr';vz='r Te';QBz='a;';
+eval "$Az$Bz$z$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$z$Az$Vz$z$Cz$Dz$z$Wz$Lz$Mz$Xz$z$Az$Vz$z$Cz$Dz$z$Ez$Fz$Gz$Hz$Yz$Zz$Hz$az$bz$bz$bz$bz$bz$bz$bz$bz$bz$bz$cz$dz$ez$fz$z$Ez$Fz$Gz$Hz$gz$hz$dz$iz$jz$kz$lz$Hz$mz$nz$Hz$gz$oz$pz$qz$z$Ez$Fz$Gz$Hz$gz$hz$dz$iz$rz$sz$lz$Hz$mz$nz$Hz$tz$uz$vz$wz$Uz$z$Ez$Fz$Gz$Hz$gz$hz$dz$iz$xz$yz$ABz$Hz$mz$nz$Hz$tz$BBz$CBz$DBz$z$Ez$Fz$Gz$Hz$gz$hz$dz$iz$EBz$FBz$lz$Hz$mz$nz$Hz$tz$GBz$CBz$HBz$z$Ez$Fz$Gz$Hz$Yz$Zz$Hz$az$bz$bz$bz$bz$bz$bz$bz$bz$bz$bz$cz$dz$ez$fz$z$Az$Vz$z$Ez$z$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$z$Az$Bz$z$Ez$Fz$Gz$Hz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$Uz$z$Az$eBz$z$Cz$Dz$z$Xz$fBz$gBz$hBz$iBz$jBz$kBz"
